0dc69f4e67c51ca8c9754b0faba7af5e4b1641b9
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-25 16:57:44 +0600

    modification

---
+++ inc/footer.php
@@ -0,0 +1,23 @@
+<div class="footersection templete clear">
+	  <div class="footermenu clear">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Privacy</a></li>
+		</ul>
+	  </div>
+	  <p>&copy; Copyright Training with live project.</p>
+	</div>
+	<div class="fixedicon clear">
+		<a href="http://www.facebook.com"><img src="images/fb.png" alt="Facebook"/></a>
+		<a href="http://www.twitter.com"><img src="images/tw.png" alt="Twitter"/></a>
+		<a href="http://www.linkedin.com"><img src="images/in.png" alt="LinkedIn"/></a>
+		<a href="http://www.google.com"><img src="images/gl.png" alt="GooglePlus"/></a>
+	</div>
+<script type="text/javascript" src="js/scrolltop.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Website</title>
+	<meta name="language" content="English">
+	<meta name="description" content="It is a website about education">
+	<meta name="keywords" content="blog,cms blog">
+	<meta name="author" content="Delowar">
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
+	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="style.css">
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+	<div class="headersection templete clear">
+		<a href="#">
+			<div class="logo">
+				<img src="images/logo.png" alt="Logo"/>
+				<h2>Website Title</h2>
+				<p>Our website description</p>
+			</div>
+		</a>
+		<div class="social clear">
+			<div class="icon clear">
+				<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+				<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+				<a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+				<a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+			</div>
+			<div class="searchbtn clear">
+			<form action="" method="post">
+				<input type="text" name="keyword" placeholder="Search keyword..."/>
+				<input type="submit" name="submit" value="Search"/>
+			</form>
+			</div>
+		</div>
+	</div>
+<div class="navsection templete">
+<nav class="navbar navbar-expand-md">
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span><i class="fas fa-bars navbar-icon custom-icon"></i></span>
+      </button>
+      
+      <a class="navbar-brand" href="#" >
+        <h5 class="logo-icon"><br>Md. Mahabubur Rahman</h5>
+        <img src="image/logo2.png" alt="">
+      </a>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</div>
+
+	
+<div class="slidersection templete clear">
+        <div id="slider">
+            <a href="#"><img src="images/slideshow/01.jpg" alt="nature 1" title="This is slider one Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/02.jpg" alt="nature 2" title="This is slider Two Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/03.jpg" alt="nature 3" title="This is slider three Title or Description" /></a>
+            <a href="#"><img src="images/slideshow/04.jpg" alt="nature 4" title="This is slider four Title or Description" /></a>
+        </div>
+
+</div>
+
+
+
+
+
+
