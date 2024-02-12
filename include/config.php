bdb1e527558d945b05bee6ff6880fc60f43bea8b
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-12 18:17:46 +0600

    error detecting

--- include/config.php
+++ include/config.php
@@ -5,12 +5,12 @@ define('DBUSER', 'root');
 define('DBPASS', '');
 define('DBNAME', 'cms');
 
-$db = new PDO("msql:host=" . DBHOST . ";dbname=" . DBNAME, DBUSER, DBPASS);
+$db = new PDO("mysql:host=" . DBHOST . ";dbname=" . DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-date_default_timezone_set('Asia / Dhaka');
+date_default_timezone_set('Asia/Dhaka'); // Corrected timezone format (no space)
 
-function __autoloadd($class)
+function my_autoload_register($class)
 {
     $class = strtolower($class);
 
@@ -26,11 +26,10 @@ function __autoloadd($class)
     if (file_exists($classpath)) {
         require_once $classpath;
     }
-
-
-
-
 }
+
+// Register the custom autoload function
+spl_autoload_register('my_autoload_register');
 
 $user = new User($db);
 
