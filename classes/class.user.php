0b9f3841b476f15969ca4b9129e12aebde50d500
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-13 10:34:29 +0600

    login error solved

--- classes/class.user.php
+++ classes/class.user.php
@@ -2,56 +2,57 @@
 class User
 {
     private $db;
+
     public function __construct($db)
     {
         $this->db = $db;
-
     }
 
     public function is_logged_in()
     {
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-            return true;
-        }
+        return isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true;
     }
+
     public function create_hash($value)
     {
-        return $hash = crypt($value, '$2a$12.substr(str_replace(' + ',' . '
-        base64_encode(sha1(microtime(true),true))),0,22)');
+        return password_hash($value, PASSWORD_DEFAULT);
     }
 
     private function verify_hash($password, $hash)
     {
-        return $hash == crypt($password, $hash);
+        return password_verify($password, $hash);
     }
+
     private function get_user_hash($username)
     {
         try {
-            $smt = $this->db->prepare('SELECT password FROM blog_user
-            WHERE username = :username');
+            $smt = $this->db->prepare('SELECT password FROM blog_user WHERE user_name = :username');
             $smt->execute(array('username' => $username));
             $row = $smt->fetch();
-            return $row['password'];
+            if ($row) {
+                return $row['password'];
+            } else {
+                return null; // User not found
+            }
         } catch (PDOException $e) {
             echo '<p class="error">' . $e->getMessage() . '</p>';
         }
     }
+
+
     public function login($username, $password)
     {
         $hashed = $this->get_user_hash($username);
-        if ($this->verify_hash($password, $hashed) == 1) {
-
+        if ($hashed !== false && $hashed !== null && $this->verify_hash($password, $hashed)) {
             $_SESSION['loggedin'] = true;
             return true;
+        } else {
+            return false;
         }
     }
     public function logout()
     {
         session_destroy();
-
     }
-
-
 }
-
 ?>
