91bb9c528c22e4a33d3ae7121e3a776557a4cdfd
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-14 22:45:17 +0600

    querty to  delete

--- Admin/index.php
+++ Admin/index.php
@@ -6,5 +6,15 @@ if (!$user->is_logged_in()) {
     header('location: login.php');
 }
 
+if (isset($_GET['delpost'])) {
+    $smt = $db->prepare('DELETE FROM blog WHERE articleId=:articleId');
+    $smt->execute(array(':articleId' => $_GET['delpost']));
+    header('location"index.php?action=deleted');
+    exit();
+
+
+}
+
+
 
 ?>
